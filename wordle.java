20e944125dc5452da3bf8fd1cb63aa64526cfc9e
manasakri <112039862+manasakri@users.noreply.github.com>
2023-10-17 22:01:38 -0700

    Update wordle.java

--- wordle.java
+++ wordle.java
@@ -7,13 +7,13 @@ public class Wordle {
 
     public void run_wordle() throws IOException {
 
-            WordList words = new WordList(); //imports WordList class which determines
-            String[] get_words = words.getWords(); //words_total is created with a list of words from words.txt
-            String wordle_answer = words.getRandomWord(get_words); // words are randomly generated to get the main wordle word
-
+            WordList words = new WordList(); 
+            String[] get_words = words.getWords(); 
+            String wordle_answer = words.getRandomWord(get_words); 
+        
             int user_guess_count = 1; //creates counter variable
             String user_next_guess; // users input
-            String temp_wordle_answer = wordle_answer; //temporary dictionary
+            String temp_wordle_answer = wordle_answer; 
 
             System.out.println("\nWelcome to Wordle!");
             System.out.println("The mystery word is a 5-letter English word.");
@@ -22,7 +22,7 @@ public class Wordle {
             while (user_guess_count <= 6) { // users can only get up to 6 guesses
                 System.out.print("\nGuess " + user_guess_count + ":");
                 Scanner scan = new Scanner(System.in);
-                user_next_guess = scan.next(); //user input for guess
+                user_next_guess = scan.next(); 
                 if (user_next_guess.length() < 5) { //checks for input words that are less than five letters
                     System.out.println("Too Short. Your guess must be 5 letters long");
                     continue;
@@ -30,17 +30,20 @@ public class Wordle {
                     System.out.println("Too Long. Your guess must be 5 letters long");
                     continue;
                 } else {
-                    if (user_next_guess.equals(temp_wordle_answer)) { //checks to see if the inputed word is exactly the same as the wordle answer
-                        System.out.print("Congrats! You guessed it!\n"); //if words are the same, game is over and exits
+                    //checks to see if the inputed word equals the wordle answer
+                    if (user_next_guess.equals(temp_wordle_answer)) { 
+                        System.out.print("Congrats! You guessed it!\n");  
                         System.exit(0);
                     } else {
-                        user_guess_count = user_guess_count + 1; // if inputed word is not the same, user gets additional guesses
+                        user_guess_count = user_guess_count + 1; 
                     }
-                    for (int i = 0; i < 5; i++) { //checks  each character in inputed word against wordle answer
-                        String character = String.valueOf(user_next_guess.charAt(i)); //each character index is represented by i
-                        if (user_next_guess.charAt(i) == temp_wordle_answer.charAt(i)) { // if the user guesses a letter at the correct spot in the wordle answer, that letter will print at that spot
+                    
+                    for (int i = 0; i < 5; i++) { 
+                        String character = String.valueOf(user_next_guess.charAt(i));  
+                        if (user_next_guess.charAt(i) == temp_wordle_answer.charAt(i)) {  
                             System.out.print(user_next_guess.charAt(i));
-                        } else if (temp_wordle_answer.contains(character)) { // if the user guesses a correct letter in the wordle answer, but at a different spot, the letter will be encased in brackets
+                        } else if (temp_wordle_answer.contains(character)) { 
+                            // if the user guesses a correct letter in the wordle answer, but at a different spot, the letter will be encased in brackets
                             System.out.print("[" + user_next_guess.charAt(i) + "]");
                         } else {
                             System.out.print("_"); // if the user did not guess a correct letter, an underscore will be used to indicate th empty positions
@@ -49,10 +52,10 @@ public class Wordle {
 
                 }
             }
-            user_guess_count = user_guess_count + 1; //moves on to the next guess after the last valid one
+            user_guess_count = user_guess_count + 1; \
 
-            while (user_guess_count > 6 ) { // if the user did not get any of the guess right ...
-                System.out.println("\nSorry! Better luck next time!\n" + "The word was " + temp_wordle_answer ); //print the correct answer 
+            while (user_guess_count > 6 ) { // 
+                System.out.println("\nSorry! Better luck next time!\n" + "The word was " + temp_wordle_answer );  
                 user_guess_count = 0; // change value to less than 6 to end while loop 
             }
             }
@@ -60,6 +63,6 @@ public class Wordle {
 
     public static void main (String args[]) throws IOException {
         Wordle wordle_obj = new Wordle();
-        wordle_obj.run_wordle(); //calls the function that checks user input
+        wordle_obj.run_wordle(); 
     }
 }
